b2b4d2280861691b0379ed1c96904b2e1d9c1e13
Guowang <lgwster@gmail.com>
2013-10-13 18:26:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var app = express();
+var fs = require('fs');
+var data = fs.readFileSync("index.html", "utf-8");
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Assignment');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
