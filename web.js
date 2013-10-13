ed54ccf2f7a46744549ab011e5cef6bab5e55d7f
Guowang <lgwster@gmail.com>
2013-10-13 18:56:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var data = fs.readFileSync("index.html", "utf-8");
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
