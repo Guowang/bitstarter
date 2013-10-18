866566c1099c8a99c4d11f58d06a89400bf157ff
Guowang <lgwster@gmail.com>
2013-10-18 01:35:50 +0000

    Added carousel.html

---
+++ webForC.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fs = require('fs');
+var data = fs.readFileSync("carousel.html", "utf-8");
+var app = express();
+
+
+app.get('/', function(request, response) {
+  response.send(data);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
